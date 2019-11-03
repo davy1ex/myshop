fc2651abd8720b0dc42904acf2740af36571c446
davy1ex <dezy1ex@gmail.com>
2019-11-03 22:50:27 +0400

    refactoring frontend

--- myshop/product/models.py
+++ myshop/product/models.py
@@ -8,5 +8,5 @@ class Product(models.Model):
     name = models.CharField(max_length=30)
     desctiption = models.CharField(max_length=100)
     type_product = models.CharField(max_length=100)
-    photo = models.FileField(upload_to="static/images/product")
+    photo = models.FileField(upload_to= ".")
     # date
